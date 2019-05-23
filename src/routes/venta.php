16fe385bf17f9010079e422ba77e5c4164b9780e
danbart <danilo_solorzano06@hotmail.com>
2019-05-23 09:56:22 -0600

    Confirmar Venta terminada

--- src/routes/venta.php
+++ src/routes/venta.php
@@ -92,16 +92,36 @@ $app->post('/api/venta/confirmar/', function(Request $request, Response $respons
     $descuento = 0;
     $totalPagar = $request->getParam('totalPagar');
     $estado = "Pendiente";
-    $codigoProd =  json_decode($request->getParam('codigoProd'));
-    var_dump($codigoProd);
-    if(isset($nit)&&isset($totalPagar)&&isset($codigoProd)){
-        //Insertamos la venta
+    $codigoProd =  json_decode($request->getParam('codigoProd'), true);
+    
+    if(isset($nit)&&isset($totalPagar)&&isset($codigoProd)&&is_array($codigoProd)){
+        //Insertamos la venta   
         $venta = "INSERT INTO venta (Fecha, NIT, Descuento, TotalPagar, Estado) VALUES ('$fecha', '$nit', '$descuento', '$totalPagar', '$estado')";
 
         try{
             $db = new db();
             $db = $db->conectar();
-            $result = $db->query($venta);
+            $db->query($venta);
+            //buscamos el codigo de la venta
+            $Pedido = $db->query("select NumPedido from venta where NIT='$nit' order by NumPedido desc limit 1");
+            $numPedido = $Pedido->fetchColumn();
+            //descontamos existensia del inventario
+            foreach($codigoProd as $numprod){
+                $prodSock = $db->query("select Stock from producto where CodigoProd='$numprod'");
+                $sockNum = $prodSock->fetchColumn();
+                if(isset($sockNum)){
+                $sockNume = (int)$sockNum;
+                //actualizamos el inventario
+                $db->query("UPDATE producto set Stock=('$sockNume'-1) where CodigoProd='$numprod'");
+                }else {
+                    $err= array('error' => 'Producto no encontrado', 'status'=> 500);
+                    return $response->withJson($err, 500);
+                }
+                
+                               
+            }
+            $data= array('message' => 'Venta Realizada con Exito', 'status'=> 200);
+            return $response->withJson($data);
             
         } catch(PDOException $e){
             $err= array('error' => $e->getMessage(), 'status'=> 500);
